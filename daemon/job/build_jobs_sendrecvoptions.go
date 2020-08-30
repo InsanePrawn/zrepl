74c349119c9cdf0c509e24a994c442b14d08b68f
InsanePrawn <insane.prawny@gmail.com>
2020-08-30 18:16:31 +0200

    zfs send/recv: Support raw sends (-w/-Lce) and property handling (-p, -b, -o, -x)
    Signed-off-by: InsanePrawn <insane.prawny@gmail.com>

--- daemon/job/build_jobs_sendrecvoptions.go
+++ daemon/job/build_jobs_sendrecvoptions.go
@@ -19,11 +19,18 @@ func buildSenderConfig(in SendingJobConfig, jobID endpoint.JobID) (*endpoint.Sen
 	if err != nil {
 		return nil, errors.Wrap(err, "cannot build filesystem filter")
 	}
-
+	sendOpts := in.GetSendOptions()
 	return &endpoint.SenderConfig{
-		FSF:     fsf,
-		Encrypt: &zfs.NilBool{B: in.GetSendOptions().Encrypted},
-		JobID:   jobID,
+		FSF:   fsf,
+		JobID: jobID,
+
+		Encrypt:              &zfs.NilBool{B: sendOpts.Encrypted},
+		SendRaw:              sendOpts.Raw,
+		SendProperties:       sendOpts.SendProperties,
+		SendBackupProperties: sendOpts.BackupProperties,
+		SendLargeBlocks:      sendOpts.LargeBlocks,
+		SendCompressed:       sendOpts.Compressed,
+		SendEmbeddedData:     sendOpts.EmbeddedData,
 	}, nil
 }
 
@@ -42,10 +49,14 @@ func buildReceiverConfig(in ReceivingJobConfig, jobID endpoint.JobID) (rc endpoi
 		return rc, errors.New("root_fs must not be empty") // duplicates error check of receiver
 	}
 
+	recvOpts := in.GetRecvOptions()
 	rc = endpoint.ReceiverConfig{
 		JobID:                      jobID,
 		RootWithoutClientComponent: rootFs,
 		AppendClientIdentity:       in.GetAppendClientIdentity(),
+
+		InheritProperties:  recvOpts.Properties.Inherit,
+		OverrideProperties: recvOpts.Properties.Override,
 	}
 	if err := rc.Validate(); err != nil {
 		return rc, errors.Wrap(err, "cannot build receiver config")
